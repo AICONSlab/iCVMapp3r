f10bcd855a56953fecc39cf6391e70a340600f27
entiri <ntiri.e@gmail.com>
2020-03-11 10:50:38 -0400

    added reference to depends manager

--- icvmapper/__init__.py
+++ icvmapper/__init__.py
@@ -4,8 +4,12 @@ import icvmapper.segment
 import icvmapper.stats
 import icvmapper.qc
 import icvmapper.utils
+import os
 
 VERSION = (0, 1, 0)
 __version__ = '.'.join(map(str, VERSION))
 
 __all__ = ['convert',  'preprocess', 'qc', 'segment', 'stats', 'utils']
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+DEPENDS_DIR = os.path.abspath(os.path.join(ROOT_DIR, "..", "depends"))
