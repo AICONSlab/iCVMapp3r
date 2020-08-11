5d69a6257d2f1ebf6ae97493c8e5d0e8b43dd8c6
entiri <ntiri.e@gmail.com>
2020-08-11 15:56:03 -0400

    updated segmentation to match hypermatter

--- icvmapper/segment/icvmapper.py
+++ icvmapper/segment/icvmapper.py
@@ -11,12 +11,13 @@ import argcomplete
 import argparse
 import numpy as np
 import nibabel as nib
-from nilearn.image import reorder_img, resample_img, resample_to_img, largest_connected_component_img, smooth_img, math_img
+from nilearn.image import reorder_img, resample_img, resample_to_img, largest_connected_component_img, smooth_img, \
+    math_img
 from nipype.interfaces.c3 import C3d
-from icvmapper.utils import endstatement
-from icvmapper.deep.predict import run_test_case
-from icvmapper.preprocess import biascorr
-from icvmapper.qc import seg_qc
+from hypermatter.utils import endstatement
+from hypermatter.deep.predict import run_test_case
+from hypermatter.preprocess import biascorr
+from hypermatter.qc import seg_qc
 import subprocess
 import warnings
 from termcolor import colored
@@ -127,7 +128,7 @@ def check_orient(in_img_file, r_orient, l_orient, out_img_file):
             orient_tag = 'RPI' if 'R' in img_ort else 'LPI'
         else:
             orient_tag = 'RPI' if 'R' in img_ort else 'LPI'
-        print("re-orienting to %s ..." %orient_tag)
+        print(orient_tag)
         orient_img(in_img_file, orient_tag, out_img_file)
         cp_orient = True
     return cp_orient
@@ -142,6 +143,27 @@ def check_orient(in_img_file, r_orient, l_orient, out_img_file):
 #     new_affine[:3, :3] = ras_image.affine[:3, :3] * np.diag(new_spacing)
 #
 #     return resample_img(ras_image, target_affine=new_affine, target_shape=output_shape, interpolation=interpolation)
+
+def cutoff_img(in_file, cutoff_percents, out_file):
+    print("\n thresholding ...")
+    img = nib.load(in_file)
+    data = img.get_data()
+    cutoff_low = np.percentile(data, cutoff_percents)
+    cutoff_high = np.percentile(data, 100-cutoff_percents)
+    print(cutoff_low)
+    print(cutoff_high)
+    new_data = data.copy()
+    new_data[new_data > cutoff_high] = cutoff_high
+    new_data[new_data < cutoff_low] = cutoff_low
+    nib.save(nib.Nifti1Image(new_data, img.affine), out_file)
+
+def normalize_sample_wise_img(in_file, out_file):
+    image = nib.load(in_file)
+    img = image.get_data()
+    # standardize intensity for data
+    print("\n standardizing ...")
+    std_img = (img - img.mean()) / img.std()
+    nib.save(nib.Nifti1Image(std_img, image.affine), out_file)
 
 def resample(img, x, y, z, out, interp=0):
     print("\n resmapling ...")
@@ -182,6 +204,9 @@ def fill_holes(in_img_file, out_img_file):
     c3.inputs.out_file = out_img_file
     c3.run()
 
+def convert(in_img_file, out_img_file):
+    subprocess.run('c3d %s -o %s' % (in_img_file, out_img_file), shell=True, stdout=subprocess.PIPE)
+
 ###########################################        Main        #########################################################
 def main(args):
     parser = parsefn()
@@ -207,28 +232,28 @@ def main(args):
         if fl is None and t2 is None:
             test_seqs = [t1]
             training_mods = ["t1"]
-            model_name = 'hfb_t1only_mcdp'
+            model_name = 'hfb_t1only_mcdp_multi'
             model_name_woc = 'hfb_t1'
             print("\n found only t1-w, using the %s model" % model_name)
 
         elif t2 is None and fl:
             test_seqs = [t1, fl]
             training_mods = ["t1", "flair"]
-            model_name = 'hfb_t1fl_mcdp'
+            model_name = 'hfb_t1fl_mcdp_multi'
             model_name_woc = 'hfb_t1fl'
             print("\n found t1 and fl sequences, using the %s model" % model_name)
 
         elif fl is None and t2:
             test_seqs = [t1, t2]
             training_mods = ["t1", "t2"]
-            model_name = 'hfb_t1t2_mcdp'
+            model_name = 'hfb_t1t2_mcdp_multi'
             model_name_woc = 'hfb_t1t2'
             print("\n found t1 and t2 sequences, using the %s model" % model_name)
 
         else:
             test_seqs = [t1, fl, t2]
             training_mods = ["t1", "flair", "t2"]
-            model_name = 'hfb_multi_mcdp'
+            model_name = 'hfb_multi_mcdp_contrast'
             model_name_woc = 'hfb'
             print("\n found all 3 sequences, using the full %s model" % model_name)
 
@@ -240,18 +265,25 @@ def main(args):
 
         # pred preprocess dir
         print(colored("\n pre-processing %s..." % os.path.abspath(subj_dir), 'green'))
-        pred_dir = "%s/pred_process" % os.path.abspath(subj_dir)
+        pred_dir = "%s/pred_process_hfb" % os.path.abspath(subj_dir)
         if not os.path.exists(pred_dir):
             os.mkdir(pred_dir)
 
+        # pred_dir_mcdp = "%s/pred_process_mcdp" % os.path.abspath(subj_dir)
+        # if not os.path.exists(pred_dir_mcdp):
+        #     os.mkdir(pred_dir_mcdp)
+
         #############
-        # bias correction if requested
         if bias is True:
+            # t1_bias = os.path.join(subj_dir, "%s_T1_nu.nii.gz" % os.path.basename(t1).split('.')[0])
             t1_bias = os.path.join(subj_dir, "%s_T1_nu.nii.gz" % subj)
             biascorr.main(["-i", "%s" % t1, "-o", "%s" % t1_bias])
             in_ort = t1_bias
         else:
-            in_ort = t1
+            in_ort = os.path.join(subj_dir, "%s.nii.gz" % os.path.basename(t1).split('.')[0])
+            in_ort = in_ort if os.path.exists(in_ort) else convert(t1, in_ort)
+            # in_ort = t1
+
 
         # std orientations
         r_orient = 'RPI'
@@ -260,6 +292,7 @@ def main(args):
         t1_ort = "%s/%s_std_orient.nii.gz" % (subj_dir, os.path.basename(t1).split('.')[0])
         if ign_ort is False:
             cp_orient = check_orient(in_ort, r_orient, l_orient, t1_ort)
+        print(cp_orient)
 
         # loading t1
         in_t1 = t1_ort if os.path.exists(t1_ort) else in_ort
@@ -267,11 +300,12 @@ def main(args):
 
         ###########
         c3 = C3d()
-        pred_shape = [128, 128, 128]
+        pred_shape = [160, 160, 160]
         test_data = np.zeros((1, len(training_mods), pred_shape[0], pred_shape[1], pred_shape[2]), dtype=t1_img.get_data_dtype())
 
         for s, seq in enumerate(test_seqs):
             print(colored("\n pre-processing %s" % os.path.basename(seq).split('.')[0], 'green'))
+
 
             seq_ort = "%s/%s_std_orient.nii.gz" % (subj_dir, os.path.basename(seq).split('.')[0])
             if training_mods[s] != 't1':
@@ -280,43 +314,33 @@ def main(args):
                 else:
                     seq_bias = os.path.join(subj_dir, "%s_T1acq_nu_T2.nii.gz" % subj)
 
-                # bias correction if requested
                 if bias is True:
                     biascorr.main(["-i", "%s" % seq, "-o", "%s" % seq_bias])
                 seq = seq_bias if os.path.exists(seq_bias) else seq
-
                 # check orientation
                 if ign_ort is False:
                     cp_orient_seq = check_orient(seq, r_orient, l_orient, seq_ort)
-
             in_seq = seq_ort if os.path.exists(seq_ort) else seq
-
-            image = nib.load(in_seq)
-            img = image.get_data()
-
-            # standardize intensity for data
-            print("\n standardizing ...")
-            std_img = (img - img.mean()) / img.std()
-            std_file = '%s/%s_standardized.nii.gz' % (pred_dir, os.path.basename(seq).split('.')[0])
-            nib.save(nib.Nifti1Image(std_img, t1_img.affine), std_file)
 
             # cropping
             if training_mods[s] == 't1':
-                crop_file = '%s/%s_standardized_cropped.nii.gz' % (pred_dir, os.path.basename(seq).split('.')[0])
-                trim(std_file, crop_file, voxels=1)
+                crop_file = '%s/%s_cropped.nii.gz' % (pred_dir, os.path.basename(seq).split('.')[0])
+                trim(in_seq, crop_file, voxels=1)
             else:
-                crop_file = '%s/%s_standardized_cropped.nii.gz' % (pred_dir, os.path.basename(seq).split('.')[0])
-                ref_file = '%s/%s_standardized_cropped.nii.gz' % (pred_dir, os.path.basename(t1).split('.')[0])
-                trim_like(std_file, ref_file, crop_file, interp=1)
+                crop_file = '%s/%s_cropped.nii.gz' % (pred_dir, os.path.basename(seq).split('.')[0])
+                ref_file = '%s/%s_cropped.nii.gz' % (pred_dir, os.path.basename(t1).split('.')[0])
+                trim_like(in_seq, ref_file, crop_file, interp=1)
 
-            #resampling
-            # img = nib.load(crop_file)
-            # res = resample(img, [pred_shape[0], pred_shape[1], pred_shape[2]])
-            # res_file = '%s/%s_resampled.nii.gz' % (pred_dir, os.path.basename(seq).split('.')[0])
-            # nib.save(res, res_file)
+            # thresholding, standardize intensity and resampling  for data
+            thresh_file = '%s/%s_cropped_thresholded.nii.gz' % (pred_dir, os.path.basename(seq).split('.')[0])
+            cutoff_percents = 5.0
+            cutoff_img(crop_file, cutoff_percents, thresh_file)
+
+            std_file = '%s/%s_cropped_thresholded_standardized.nii.gz' % (pred_dir, os.path.basename(seq).split('.')[0])
+            normalize_sample_wise_img(thresh_file, std_file)
 
             res_file = '%s/%s_resampled.nii.gz' % (pred_dir, os.path.basename(seq).split('.')[0])
-            resample(crop_file, pred_shape[0], pred_shape[1], pred_shape[2], res_file, interp=1)
+            resample(std_file, pred_shape[0], pred_shape[1], pred_shape[2], res_file, interp=1)
 
             if not os.path.exists(res_file):
                 print("\n pre-processing %s" % training_mods[s])
@@ -336,6 +360,7 @@ def main(args):
             pred = run_test_case(test_data=test_data, model_json=model_json, model_weights=model_weights,
                                  affine=res.affine, output_label_map=True, labels=1)
             pred_s[sample_id, :, :, :] = pred.get_data()
+            #nib.save(pred, os.path.join(pred_dir_mcdp, "hfb_pred_%s.nii.gz" % sample_id))
 
         # computing mean
         pred_mean = pred_s.mean(axis=0)
@@ -349,7 +374,7 @@ def main(args):
         nib.save(pred_th, pred_th_name)
 
         # resample back
-        pred_res = resample_to_img(pred_prob, t1_img)
+        pred_res = resample_to_img(pred_prob, t1_img, interpolation="linear")
         pred_prob_name = os.path.join(pred_dir, "%s_%s_pred_prob.nii.gz" % (subj, model_name))
         nib.save(pred_res, pred_prob_name)
 
@@ -362,6 +387,7 @@ def main(args):
         nib.save(pred_comp, pred_name)
 
         # copy original orientation to final prediction
+        print(cp_orient)
         if ign_ort is False and cp_orient:
             nib.save(pred_comp, prediction_std_orient)
             fill_holes(prediction_std_orient, prediction_std_orient)
@@ -386,7 +412,7 @@ def main(args):
             nib.save(masked_t1_std, t1_masked_name_std)
 
         # predict cerebellum
-        if woc == 1 and model_name_woc == 'hfb':
+        if woc == 1:
             print("\n predicting approximate cerebellar mask")
 
             cereb_prediction = '%s/%s_T1acq_nu_cerebellum_pred.nii.gz' % (subj_dir, subj) \
@@ -441,14 +467,13 @@ def main(args):
                     if bias is True else '%s/%s_masked_woc_std_orient.nii.gz' % (subj_dir, os.path.basename(t1).split('.')[0])
                 woc_t1 = math_img("img1 * img2", img1=t1_img, img2=nib.load(woc_pred_std_orient))
                 nib.save(woc_t1, t1_woc_name)
-        else:
-            print('Removing cerebellum is available only when all 3 sequence t1, flair, and t2 have existed.')
 
         print("\n generating mosaic image for qc")
 
         seg_qc.main(["-i", "%s" % t1, "-s", "%s" % prediction, "-g", "5", "-m", "75"])
 
         endstatement.main('Brain extraction and mosaic generation', '%s' % (datetime.now() - start_time))
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
